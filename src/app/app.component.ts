16dd6b2fa459f9875c75e9cf4156ae4fc3409c2e
Tresor-ilunga <19im065@esisalama.org>
2023-06-30 15:02:18 +0200

    perf: View data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+
+  ngOnInit() {
+    this.title = 'Angular';
+    this.description = 'Angular is a platform for building mobile and desktop web applications.';
+    this.createDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
 }
